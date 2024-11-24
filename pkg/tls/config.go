4a8315b48bdb8e2bf5d5b58e84738a6cbf99174d
Mohammed Naser <mnaser@vexxhost.com>
2024-11-23 21:50:25 -0500

    Add config option pattern

--- pkg/tls/config.go
+++ pkg/tls/config.go
@@ -12,8 +12,40 @@ type Config struct {
 	OnUpdate   func()
 }
 
-func NewConfig() (*Config, error) {
-	return &Config{
+type Option func(*Config)
+
+func WithRestConfig(restConfig *rest.Config) Option {
+	return func(c *Config) {
+		c.RestConfig = restConfig
+	}
+}
+
+func WithTemplate(template *template.Template) Option {
+	return func(c *Config) {
+		c.Template = template
+	}
+}
+
+func WithPaths(paths *WritePathConfig) Option {
+	return func(c *Config) {
+		c.Paths = paths
+	}
+}
+
+func WithOnUpdate(onUpdate func()) Option {
+	return func(c *Config) {
+		c.OnUpdate = onUpdate
+	}
+}
+
+func NewConfig(opts ...Option) (*Config, error) {
+	config := &Config{
 		OnUpdate: func() {},
-	}, nil
+	}
+
+	for _, opt := range opts {
+		opt(config)
+	}
+
+	return config, nil
 }
